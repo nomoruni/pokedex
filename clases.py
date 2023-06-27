acec1944e75b7b3a4d556a180e964671ffd4a89f
nomoruni <bryanvaldespino1999@gmail.com>
2023-06-26 22:45:36 -0400

    On branch master Changes to be committed: 	new file:   __pycache__/clases.cpython-311.pyc 	new file:   amount 	modified:   clases.py 	new file:   db/1 	modified:   main.py

--- clases.py
+++ clases.py
@@ -2,35 +2,37 @@ import json
 from os import remove, rename
 
 t_tipo=["planta", "fuego", "agua", "rayo", "lucha", "insecto", "siniestro", "psiquico", "tierra", "roca", "acero", "dragon", "hada", "hielo", "normal", "volador", "veneno"]
-t_nat=["activa", "afable", "agitada", "alegre", "alocada", "amable", "audaz", "cauta", "dócil", "firme", "floja", "fuerte", "grosera", "huraña", "ingenua", "mansa", "miedosa", "modesta", "osada", "pícara", "rara", "serena", "seria", "tímida"]
+#t_nat=["activa", "afable", "agitada", "alegre", "alocada", "amable", "audaz", "cauta", "dócil", "firme", "floja", "fuerte", "grosera", "huraña", "ingenua", "mansa", "miedosa", "modesta", "osada", "pícara", "rara", "serena", "seria", "tímida"]
 
 class Pokemon():
-    def __init__(self, nombre, tipo, tipo2, genero, descripcion):
+    def __init__(self, nombre, tipo, tipo2, descripcion):
         self.nombre = nombre.title()
         self.tipo = str(t_tipo[tipo - 1])
-        self.tipo2 = str(t_tipo[tipo2 - 1])
-        self.genero = genero
+        if int(tipo2) != 0:
+            self.tipo2 = str(t_tipo[tipo2 - 1])
+        else:
+            self.tipo2 = "-"
         self.descripcion = descripcion
         
     def __str__(self):
-        return "Nombre: " + self.nombre + "Tipo: " + self.tipo + "Genero: " + self.genero + "Naturaleza: " + self.naturaleza + "Descripción: " + self.descripcion + "\n"
+        return " Nombre: " + self.nombre + " Tipo #1: " + self.tipo + " Tipo #2: " + self.tipo2 + " Descripción: " + self.descripcion + "\n"
 
 class Pokedex:
     
     def __init__(self):
         try:
-            with open("amount.txt") as num:
+            with open("amount") as num:
                 self.numero = int(num.read())
             self.pokemons = []
         except FileNotFoundError:
-            with open("amount.txt", "w") as num:
+            with open("amount", "w") as num:
                 num.write("0")
             self.numero = 0
+            self.pokemons = []
         
         
     def Insertar_Pokemon(self, pokemon):
         self.pokemons.append(pokemon)
-        self.numero += 1
         print("Pokemon Agregado a la Pokedex")
         
     def Borrar_Pokemon_Indice(self, indice):
@@ -61,24 +63,24 @@ class Pokedex:
     def Escribir_Archivo_Pokemons(self):
         
         for poke in self.pokemons:
-            with open("db/" + str(self.numero), "w") as file_object:
+            with open("db/" + str(self.numero+1), "w") as file_object:
                 file_object.write(poke.__str__())
                 self.numero += 1
                 
         with open("amount", "w") as num:
-            num.write(self.numero)
+            num.write(str(self.numero))
                 
         print("Base de datos guardada")
     
     def __str__(self):
         try:
-            for poke in range(0, self.numero+1):            
+            for poke in range(1, self.numero+1):            
                 with open("db/" + str(poke)) as file_text:
-                    content += file.read()
+                    content = file_text.read()
+                    print("No: " + str(poke) + " " +  content + "\n")
         except FileNotFoundError:
-            raise print("El archivo no existe!!")
-        else:
-            return content
+            print("El archivo no existe!!")
+            return 0
     
     
             
--- main.py
+++ main.py
@@ -12,49 +12,42 @@ def main():
         num = int(input("Ingrese un número de 1-3:"))
         
         if num == 1:
-            try:
-                with open("show.txt") as show:
-                    info = show.read()
-            except FileNotFoundError:
-                print("No existe ningun pokemon en la pokedex aún. Cree uno!")
-
+            if pokedex.__str__() == 0:
+                num = 0
+                print("No existe ningun pokemon en la pokedex aún. Cree uno!\n \n")
+            num = 0
+            
         if num == 2:
             nombre = input("Inserte el nombre del pokemon: \n")
             
             tipo = input("Elija el tipo: \n 1-planta\n 2-fuego\n 3-agua\n 4-rayo\n 5-lucha\n 6-insecto\n 7-siniestro\n 8-psiquico\n 9-tierra\n 10-roca\n 11-acero\n 12-dragon\n 13-hada\n 14-hielo\n 15-normal\n 16-volador\n 17-veneno\n")
             lista = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "11", "12", "13", "14", "15", "16", "17"]
             
-            tipo2 = input("Entre el tipo secundario si tiene solo uno entre 0: \n 1-planta\n 2-fuego\n 3-agua\n 4-rayo\n 5-lucha\n 6-insecto\n 7-siniestro\n 8-psiquico\n 9-tierra\n 10-roca\n 11-acero\n 12-dragon\n 13-hada\n 14-hielo\n 15-normal\n 16-volador\n 17-veneno\n")
+            tipo2 = input("Entre el tipo secundario, si tiene solo uno entre 0: \n 1-planta\n 2-fuego\n 3-agua\n 4-rayo\n 5-lucha\n 6-insecto\n 7-siniestro\n 8-psiquico\n 9-tierra\n 10-roca\n 11-acero\n 12-dragon\n 13-hada\n 14-hielo\n 15-normal\n 16-volador\n 17-veneno\n")
             
             if tipo not in lista or tipo == 0 or tipo2 not in lista:  
                 print("Debe entrar un número valido!! \n")
                 num = 0
                 continue    
-            
-            genero = input("Elija un Genero: \n 1-Masculino\n 2-Femenino\n")
-            
-            if genero == "1":
-                genero = "M"
-            elif genero == "2":
-                genero == "F"
-            else: 
-                print("Debe escribir un número entre 1 y 2")
-                num = 0
-                continue
+            elif tipo2 == tipo:
+                tipo2 = 0
             
             descripcion = input("Por ultimo, escriba una breve descripción del Pokemon: \n")
             
             pokemon = Pokemon(nombre, int(tipo), int(tipo2), genero, descripcion)
             pokedex.Insertar_Pokemon(pokemon)
             pokedex.Escribir_Archivo_Pokemons()
+            num = 0
         if num == 3:
             choise = input("Que metodo desea usar: \n 1-Borrar por número de Pokedex \n 2-Borrar por nombre")
             if choise == 1:
                 indice = input("Entre el número de Pokemon: \n")
                 pokedex.Borrar_Pokemon_Indice(indice)
+                num = 0
             if choise == 2:
                 nombrep = input("Entre el nombre del Pokemon: \n")
                 pokedex.Borrar_Pokemon_Nombre(nombrep)
+                num = 0
             else: 
                 print("Escoja una opcion válida!!")
                 num = 0
